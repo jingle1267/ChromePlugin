28432c77446b61c68db222bfc77241b7326ae4dc
jingle1267 <jingle1267@163.com>
2019-02-12 17:22:07 +0800

    [ADD] 优化刷新方法，不用打开关闭新窗口；

--- js/background.js
+++ js/background.js
@@ -1,18 +1,15 @@
 console.log('background.js run');
 
-var OpenTimes1 = 1;
+var OpenTimes = 1;
 
 // 打开隐身窗口的时间
 var OpenDuration = 3123;
-// 隐身窗口打开后，多久关闭
-var CloseDuration = 2202;
 
 // 百度搜索
 chrome.contextMenus.create({
     title: '百度搜索 %s', // %s表示选中的文字
     contexts: ['selection'], // 只有当选中文字时才会出现此右键菜单
-    onclick: function(params)
-    {
+    onclick: function (params) {
         // 注意不能使用location.href，因为location是属于background的window对象
         chrome.tabs.create({url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(params.selectionText)});
     }
@@ -20,91 +17,145 @@ chrome.contextMenus.create({
 
 // 必应搜索
 chrome.contextMenus.create({
-    title: '必应搜索 %s', 
-    contexts: ['selection'], 
-    onclick: function(params)
-    {
+    title: '必应搜索 %s',
+    contexts: ['selection'],
+    onclick: function (params) {
         chrome.tabs.create({url: 'https://cn.bing.com/search?q=' + encodeURI(params.selectionText)});
     }
 });
 
 // 百度翻译
 chrome.contextMenus.create({
-    title: '百度翻译 %s', 
-    contexts: ['selection'], 
-    onclick: function(params)
-    {
+    title: '百度翻译 %s',
+    contexts: ['selection'],
+    onclick: function (params) {
         chrome.tabs.create({url: 'https://fanyi.baidu.com/#en/zh/' + encodeURI(params.selectionText)});
     }
 });
 
-// 无痕模式打开1次
+// 无痕模式打开
 chrome.contextMenus.create({
-    title: "无痕模式打开当前页面" + OpenTimes1 + "次",
-    contexts: ['page', 'image', 'video', 'frame'], 
-    onclick: function(){
-		openIncognitoWindow(OpenTimes1, false)
-	}
+    title: "无痕打开",
+    contexts: ['page', 'image', 'video', 'frame'],
+    onclick: function () {
+        openIncognitoWindow()
+    }
 });
 
-// 获取用户设置的打开次数。可以在 options 中设置
-var defaultConfig = {iot: 1}; // 默认配置
-chrome.storage.sync.get(defaultConfig, function(result) {
-	var customOpenTimes = result.iot;
-    console.log('Value currently is ' + customOpenTimes);
-
-    if (customOpenTimes != 1) {
-        chrome.contextMenus.create({
-            title: "无痕模式打开当前页面" + customOpenTimes + "次",
-            onclick: function(){
-                openIncognitoWindow(customOpenTimes, true)
-            }
-        });
-	}
-
+// triggered when user clicks on installed extention icon
+chrome.browserAction.onClicked.addListener(function (tab) {
+    trueRefresh(tab);
 });
+
+// 添加刷新当前页面菜单
+getIOT(function (customOpenTimes) {
+    OpenTimes = customOpenTimes;
+
+    let title = '清 cookies 刷新';
+    if (OpenTimes != 1) {
+        title += ' ' + OpenTimes + ' 次';
+    }
+    chrome.contextMenus.create({
+        title: title,
+        contexts: ['page', 'image', 'video', 'frame'],
+        onclick: function () {
+            var index = 0;
+            var interval = setInterval(function () {
+                getCurrentTab(function (tab) {
+                    trueRefresh(tab);
+                });
+                index++;
+                if (index >= OpenTimes) {
+                    clearInterval(interval);
+                }
+            }, OpenDuration);
+
+        }
+    });
+});
+
+/**
+ * 清空数据并刷新
+ * @param tab
+ */
+function trueRefresh(tab) {
+    // retrive domain from active tab
+    var url = tab.url;
+    var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    var d = matches && matches[1].replace('www.', '');
+    d = '.' + d;
+
+    // get all cookies for domain
+    chrome.cookies.getAll({domain: d}, function (cookies) {
+
+        // iterate on cookie to get cookie detail
+        for (var i = 0; i < cookies.length; i++) {
+            var url = "http" + (cookies[i].secure ? "s" : "") + "://" + cookies[i].domain + cookies[i].path;
+            var cname = cookies[i].name;
+
+            // delete cookie
+            chrome.cookies.remove({
+                "url": url,
+                "name": cname
+            });
+        }
+
+        // reload currect active tab
+        chrome.tabs.reload();
+    });
+}
+
 
 /**
  * 打开隐身状态的窗口
  */
-function openIncognitoWindow(openTimes, auto) {
-	var url = chrome.windows.getCurrent({"populate":true}, function (window) {
-		console.log("window = ", window);
+function openIncognitoWindow() {
 
-		for (var i = 0; i < window.tabs.length; i++) {
-			var tab = window.tabs[i];
+    getCurrentTab(function (tab) {
+        if (tab.active) {
 
-			if (tab.active) {
-                var url = tab.url;
-				for (var j = 0; j < openTimes; j++) {
-					(function(e) {setTimeout(function() {
-						console.log("e = ", e, "-", new Date());
+            var params = {"url": tab.url, "incognito": true};
+            chrome.windows.create(params, function (wd) {
+                    console.log('wd : ', wd, "-", new Date());
+                }
+            );
 
-						var params;
-						if (auto) {
-                            params = {"url": url, "incognito": true, "width": 400,
-                                "height": 300};
-                        } else {
-                            params = {"url": url, "incognito": true};
-						}
-						chrome.windows.create(params, function (wd) {
-								console.log('wd : ', wd, "-", new Date());
-								if (auto) {
-                                    setTimeout(function() {
-                                        chrome.windows.remove(wd.id);
-                                        console.log('close window : ', new Date());
-                                    }, CloseDuration);
-                                }
+        }
+    });
 
-							}
-						);
-					}, e * OpenDuration)})(j);
-			
-				}
-				break;
-			}
-		}
+}
 
-	});
-	console.log("url = ", url);
+/**
+ * 获取无痕模式打开次数
+ * @param callback
+ */
+function getIOT(callback) {
+    // 获取用户设置的打开次数。可以在 options 中设置
+    var defaultConfig = {iot: 1}; // 默认配置
+    chrome.storage.sync.get(defaultConfig, function (result) {
+        var customOpenTimes = result.iot;
+        console.log('Value currently is ' + customOpenTimes);
+
+        callback(customOpenTimes);
+    });
+}
+
+/**
+ * 获取当前 Tab
+ * @param callback
+ */
+function getCurrentTab(callback) {
+    chrome.windows.getCurrent({"populate": true}, function (window) {
+        console.log("window = ", window);
+
+        for (var i = 0; i < window.tabs.length; i++) {
+            var tab = window.tabs[i];
+
+            if (tab.active) {
+                callback(tab);
+                break;
+            }
+        }
+
+    });
 }
